05b745e63f917e3860075a7d39b60de89701ac97
Jay Kansara <maxthonpc18@gmail.com>
2023-03-28 16:09:43 +0530

    upload to Firebase App Distribution

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Text, View, StyleSheet} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+
 const App = () => {
   const [message, setMessage] = useState('');
+  const getFCMToken = async () => {
+    try {
+      const token = await messaging().getToken();
+      console.log(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getFCMToken();
+  }, []);
   return (
     <View style={{flex: 1, justifyContent: 'center'}}>
       <Button
